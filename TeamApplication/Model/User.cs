5869a56f7a35821967fc911cffd5568e541d6f66
aydinnasibli <aydinnasibli7@gmail.com>
2021-07-03 21:57:20 +0400

    Second Committ

--- TeamApplication/Model/User.cs
+++ TeamApplication/Model/User.cs
@@ -1,7 +1,21 @@
-﻿namespace TeamApplication.Model
+﻿using System.Runtime.CompilerServices;
+
+namespace TeamApplication.Model
 {
     public class User
     {
+        public string username { get; set; }
         
+        public string password { get; set; }
+        public string name { get; set; }
+        public string surname{ get; set; }
+        public int age{ get; set; }
+        public string phone_number{ get; set; }
+        public string mail{ get; set; }
+
+        public User()
+        {
+            
+        }
     }
 }
--- TeamApplication/ViewModel/BaseViewModel.cs
+++ TeamApplication/ViewModel/BaseViewModel.cs
@@ -1,14 +1,99 @@
-﻿using TeamApplication.Model;
-
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using System.Windows;
+using System.Windows.Input;
+using Newtonsoft.Json;
+using TeamApplication.Commands;
+using TeamApplication.Model;
+using System.Text.RegularExpressions;
 namespace TeamApplication.ViewModel
 {
     public class BaseViewModel
     {
-        private readonly BaseModel _baseModel;
+        public RelayCommand mycmd { get; set; }
+        public ICommand CloseCommand { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        
+        public string username { get; set; }
+        
+        public string password { get; set; }
+        public string Mail { get; set; }
+        List<User> users = new List<User>();
 
-        public BaseViewModel(BaseModel baseModel)
+        public BaseViewModel()
         {
-            _baseModel = baseModel;
+            
+            
+            CloseCommand = new RelayCommand(o =>
+            {
+                System.Windows.Application.Current.Shutdown();
+            }, o => true);
+            
+            mycmd = new RelayCommand(o =>
+            {
+                User user = new User();
+                user.name = Name;
+                user.surname = Surname;
+                user.age = Age;
+                user.mail = Mail;
+                user.phone_number = Phone;
+                user.password = password;
+                user.username = username;
+                ValidateProps();
+                
+                if(username!=null && Name!=null && Surname!=null && password!=null && Phone!=null && Mail!=null)
+                {
+                    string json = JsonConvert.SerializeObject(users, Formatting.Indented);
+                    File.WriteAllText(@"C:\Users\aydin\RiderProjects\TeamApplication\TeamApplication\Resources\users.txt",
+                        json);
+                    users.Add(user);
+                }
+                
+               
+                
+                
+                
+            }, o => true);
         }
+
+        private void ValidateProps()
+        {
+            if (Regex.IsMatch(Mail, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")==false)
+            {
+                MessageBox.Show("Write an email adress!!!!!!!!!!!!!");
+                 return;
+            }
+            else if (Name == null)
+            {
+                MessageBox.Show("I suppose you have name!");
+
+            }
+            else if (Surname == null)
+            {
+                MessageBox.Show("Of course a Surname tooo");
+
+            }
+            else if (username == null)
+            {
+                MessageBox.Show("Wanna get  username for yourself");
+
+            }
+            else if (password == null)
+            {
+                MessageBox.Show("U can`t get in without password");
+
+            }
+            else if (Phone == null)
+            {
+                MessageBox.Show("Go buy a phone number and after come back pls ty");
+
+            }
+        }
+        
     }
+    
 }
