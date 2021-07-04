49bb177380548ef385e4e16b3c85236c110b4e40
OmerHaciyev-o4 <omer04118102@gmail.com>
2021-07-03 19:58:12 -0700

    Class for deseralize

--- TeamApplication/Model/BaseModel.cs
+++ TeamApplication/Model/BaseModel.cs
@@ -1,8 +1,25 @@
-﻿namespace TeamApplication.Model
+﻿using Newtonsoft.Json;
+
+namespace TeamApplication.Model
 {
     public class BaseModel
     {
-        public string BaseString { get; set; }
-        public int BaseInt { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("name")]
+        public string Password { get; set; }
+
+        [JsonProperty("surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty("age")]
+        public int Age { get; set; }
+
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("mail")]
+        public string Mail { get; set; }
     }
 }
