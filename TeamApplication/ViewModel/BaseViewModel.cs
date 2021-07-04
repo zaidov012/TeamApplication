9a874c58e041816384954c3bb8b055df09097e44
OmerHaciyev-o4 <omer04118102@gmail.com>
2021-07-03 22:17:49 -0700

    UI background works

--- TeamApplication/ViewModel/BaseViewModel.cs
+++ TeamApplication/ViewModel/BaseViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Windows;
 using System.Windows.Input;
 using TeamApplication.Commands;
 using TeamApplication.Model;
+using System.Collections.ObjectModel;
+using System.Text.Json;
 using Newtonsoft.Json;
 
 namespace TeamApplication.ViewModel
@@ -10,7 +13,7 @@ namespace TeamApplication.ViewModel
     public class BaseViewModel
     {
         public string Username { get; set; }
-        public string Password { get; set; }
+        public string Password { get; set; } 
         private List<BaseModel> _users { get; set; }
         public ICommand CloseCommand { get; set; }
         private object _empty;
@@ -24,20 +27,21 @@ namespace TeamApplication.ViewModel
 
         public BaseViewModel()
         {
+            _users = new List<BaseModel>();
             _empty = null;
             CloseCommand = new RelayCommand(o =>
             {
                 System.Windows.Application.Current.Shutdown();
             }, o => true);
 
-            ReadToFile();
+           ReadToFile();
         }
 
         private void ReadToFile()
         {
-            var info = File.ReadAllLines("users.txt");
+            var info = File.ReadAllText("../../../Resource/users.txt");
             if (info == null) return;
-            for (int i = 0; i < info.Length; i++)  { _users.Add(JsonConvert.DeserializeObject<BaseModel>(info[i])); }
+            _users = JsonConvert.DeserializeObject<List<BaseModel>>(info);
         }
 
         public void CheckInfo(object empty)
@@ -45,9 +49,9 @@ namespace TeamApplication.ViewModel
             if (Username == null && Password == null) return;
             for (int i = 0; i < _users.Count; i++)
             {
-                if (Username == _users[i].Name && Password == _users[i].Password)
+                if (Username == _users[i].username && Password == _users[i].password)
                 {
-                    // Shemsen
+                    // TODO: next work Shemsedin.
                     return;
                 }
             }
