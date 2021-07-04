aff2848b90f2c9f935d2b4f7462a610d97bbcc0b
OmerHaciyev-o4 <omer04118102@gmail.com>
2021-07-03 20:04:29 -0700

    UI background works

--- TeamApplication/ViewModel/BaseViewModel.cs
+++ TeamApplication/ViewModel/BaseViewModel.cs
@@ -1,14 +1,56 @@
-﻿using TeamApplication.Model;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows.Input;
+using TeamApplication.Commands;
+using TeamApplication.Model;
+using Newtonsoft.Json;
 
 namespace TeamApplication.ViewModel
 {
     public class BaseViewModel
     {
-        private readonly BaseModel _baseModel;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        private List<BaseModel> _users { get; set; }
+        public ICommand CloseCommand { get; set; }
+        private object _empty;
+        private ICommand? _submitCommand;
 
-        public BaseViewModel(BaseModel baseModel)
+        public ICommand SubmitCommand
         {
-            _baseModel = baseModel;
+            set => _submitCommand = value;
+            get { return _submitCommand ??= new RelayCommand(CheckInfo); }
+        }
+
+        public BaseViewModel()
+        {
+            _empty = null;
+            CloseCommand = new RelayCommand(o =>
+            {
+                System.Windows.Application.Current.Shutdown();
+            }, o => true);
+
+            ReadToFile();
+        }
+
+        private void ReadToFile()
+        {
+            var info = File.ReadAllLines("users.txt");
+            if (info == null) return;
+            for (int i = 0; i < info.Length; i++)  { _users.Add(JsonConvert.DeserializeObject<BaseModel>(info[i])); }
+        }
+
+        public void CheckInfo(object empty)
+        {
+            if (Username == null && Password == null) return;
+            for (int i = 0; i < _users.Count; i++)
+            {
+                if (Username == _users[i].Name && Password == _users[i].Password)
+                {
+                    // Shemsen
+                    return;
+                }
+            }
         }
     }
 }
